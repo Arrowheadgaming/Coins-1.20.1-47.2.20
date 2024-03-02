382f18b9e6e2e6cfd4635e127cda3f8bfa364427
Arrowheadgaming <arrowheadgaming03@gmail.com>
2024-03-02 03:12:59 -0600

    Add JEI

---
+++ src/main/java/net/arrowheadgaming/coins/compat/JEICoinsModPlugin.java
@@ -0,0 +1,32 @@
+package net.arrowheadgaming.coins.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.arrowheadgaming.coins.Coins;
+import net.minecraft.resources.ResourceLocation;
+
+@JeiPlugin
+public class JEICoinsModPlugin implements IModPlugin{
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(Coins.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IModPlugin.super.registerCategories(registration);
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        IModPlugin.super.registerRecipes(registration);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        IModPlugin.super.registerGuiHandlers(registration);
+    }
+}
